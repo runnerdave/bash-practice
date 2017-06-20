2d9d7588499b79f18a1b116c7f7980155f77fe93
David Jimenez <david.jimenez@rmit.edu.au>
2017-06-20 11:53:16 +1000

    practicing functions in bash

--- functions/hello.sh
+++ functions/hello.sh
@@ -5,7 +5,13 @@ function quit {
 function e {
 	echo $1 
 }  
+function returnSomething {
+	local myresult="--$1--"
+	echo "$myresult"
+}
 e Hello
 e World
+result=$(returnSomething holas)
+echo $result
 quit
 echo foo 
